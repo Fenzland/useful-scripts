65d37e9e7abab33ca616e32559f0d6d35e9895f1
Fenz <uukooo@126.com>
2019-07-31 23:30:04 +0800

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+alias v=vim
+alias g=git
+alias ㄍ=git
+
+alias net=kde5-nm-connection-editor
+
+#alias -='cd -'
+
+set GOPATH $HOME/go
+set GOROOT /usr/local/go
+set PATH $PATH $GOROOT/bin $GOPATH/bin
+
+
+set PATH $PATH ./vendor/bin $HOME/.composer/vendor/bin
+
+set PATH $PATH $HOME/.deno/bin
+
+set PATH . $PATH
